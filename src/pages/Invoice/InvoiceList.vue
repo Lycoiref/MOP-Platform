4f42f743ba17da38c7eb86dfc0fc91a8eb8a1130
Lycoiref <1808347356@qq.com>
2023-04-19 20:11:25 +0800

    添加新增发票审批按钮

--- src/pages/Invoice/InvoiceList.vue
+++ src/pages/Invoice/InvoiceList.vue
@@ -5,12 +5,26 @@
         <InvoiceCard></InvoiceCard>
         <InvoiceCard></InvoiceCard>
         <InvoiceCard></InvoiceCard>
+        <div class="add">
+            <div class="add_icon" @click="addInvoice">
+                <el-icon><Plus /></el-icon>
+            </div>
+            新增发票审批
+        </div>
     </div>
 </template>
 
 <script setup>
 import InvoiceCard from '@/components/InvoiceCard.vue'
 import MobileHeader from '@/components/MobileHeader.vue'
+import { Plus } from '@element-plus/icons-vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const addInvoice = () => {
+    router.push('/invoice/add')
+}
 </script>
 
 <style scoped lang="less">
@@ -20,5 +34,29 @@ import MobileHeader from '@/components/MobileHeader.vue'
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    .add {
+        position: fixed;
+        right: 20px;
+        bottom: 20px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        color: #66ccff;
+    }
+
+    .add_icon {
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+        margin-bottom: 10px;
+        background-color: #66ccff;
+        color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 40px;
+    }
 }
 </style>
